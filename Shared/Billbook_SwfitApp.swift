0a1ad313b135545ecb173c450be00bf293007cf9
iTh1nk <fnchao@hotmail.com>
2020-12-26 20:40:02 -0800

    Initial Commit

---
+++ Shared/Billbook_SwfitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Billbook_SwfitApp.swift
+//  Shared
+//
+//  Created by Chao Feng on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Billbook_SwfitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
